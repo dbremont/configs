bc589820ac4bbe4be67f08efe7b57ca412d728ea
dbremont <darlynvictoriano@gmail.com>
2023-09-05 11:23:47 -0400

    cambios masivos

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -35,3 +35,34 @@ there="$HOME/.shell.here"
 there() {
     cd "$(readlink "${there}")"
 }
+
+
+setTerminalText () {
+    # echo works in bash & zsh
+    local mode=$1 ; shift
+    echo -ne "\033]$mode;$@\007"
+}
+
+# Like a local wormhole
+function snd() {
+    mkdir -p /tmp/passage
+    /bin/cp $1 /tmp/passage
+    /bin/rm /tmp/passage/last &>/dev/null || true
+    echo $1 > /tmp/passage/last
+}
+
+function rcv() {
+    file=$(cat /tmp/passage/last)
+    /bin/cp -i "/tmp/passage/$file" $file
+    /bin/rm "/tmp/passage/$file"
+}
+
+
+# Complete from google
+function auto() {
+    url='https://www.google.com/complete/search?client=hp&hl=en&xhr=t'
+    # NB: user-agent must be specified to get back UTF-8 data!
+    curl -H 'user-agent: Mozilla/5.0' -sSG --data-urlencode "q=$*" "$url" |
+        jq -r ".[1][][0]" |
+        sed 's,</\?b>,,g'
+}
